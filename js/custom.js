1f3cc7cb7251ae514aa87753767371fd69b17f46
Mohammad Rahat <86203884+developerrahat@users.noreply.github.com>
2022-09-25 23:31:51 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+// Input_ Button Animation Js===========
+document.querySelector(".my_custom_style").addEventListener('focus', test);
+
+function test(){
+   document.querySelector(".my_label").style.top = '25px';
+   document.querySelector(".my_label").style.fontSize = '14px';
+
+}
+document.querySelector(".my_custom_style2").addEventListener('focus', test2);
+
+function test2(){
+   document.querySelector(".my_label2").style.top = '25px';
+   document.querySelector(".my_label2").style.fontSize = '14px';
+
+}
+document.querySelector(".my_custom_style3").addEventListener('focus', test3);
+
+function test3(){
+   document.querySelector(".my_label3").style.top = '25px';
+   document.querySelector(".my_label3").style.fontSize = '14px';
+
+}
